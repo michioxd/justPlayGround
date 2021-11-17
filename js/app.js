ec95a9efcea300e44d777f79dfac6f308a42c23f
michio <80969068+michioxd@users.noreply.github.com>
2021-11-17 10:31:30 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -173,14 +173,14 @@ $(document).ready(function() {
                 var UserLastCode = localStorage.getItem('UserLastCode');
                 if (UserLastCode !== Value) {
                     mdui.confirm('Bạn chưa lưu lại các thay đổi, bạn có muốn refresh lại trang không?', 'Chắc chưa? :)', function() {
-                        window.location.href = "";
+                        window.location.href = "/";
                     });
                     return false;
                 } else {
                     mdui.snackbar({
                         message: "Đang reload..."
                     })
-                    window.location.href = "";
+                    window.location.href = "/";
                 }
                 return false;
             }
